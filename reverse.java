2785a6523557fa9e12e5445ebb0d0a7af1390a59
Appu ab <50874934+appu22@users.noreply.github.com>
2021-01-19 06:02:29 +0530

    write a program to reverse a number in java
    1)write a program to reverse a number in java 
    2)write a program to reverse a number in java with using  using built on functions

--- reverse.java
+++ reverse.java
@@ -1,4 +1,8 @@
-package appuuuu;
+/*
+1) write a program to reverse a number in java without using built in funcitions
+2) write a program to reverse a number in java 
+*/
+appuuuu;
 
 import java.util.Scanner;
 
@@ -15,7 +19,7 @@ public static void main (String[] args)
 		System.out.println("Enter a number");
 		
 		num=sc.nextInt();
-		
+		//logic
 		int org=num;
 		
 		rev=0;
@@ -28,3 +32,49 @@ public static void main (String[] args)
 		System.out.println("Reverse number is :"+rev);
 }
 }
+// program - 2
+package reverse;
+
+import java.util.Scanner;
+
+public class reverse2 
+{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		
+		//logic
+		//String Buffer class
+		int rev;
+		StringBuffer sb = new StringBuffer(String.valueOf(num));
+		StringBuffer rev1=sb.reverse();	
+		System.out.println("Reverse number is "+rev1);
+	}
+
+}
+//logic - 3
+package reverse;
+
+import java.util.Scanner;
+
+public class reverse2 
+{
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = sc.nextInt();
+		
+		//logic
+		//String Buffer class
+		int rev;
+		StringBuilder sb = new StringBuilder(); //(String.valueOf(num));
+		sb.append(num);
+		StringBuilder rev1=sb.reverse();	
+		System.out.println("Reverse number is "+rev1);
+	}
+
+}
+
