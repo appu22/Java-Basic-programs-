8ffd26a73909fdaa2b954eaaffaa0844cd106ea7
{CodeZone} <50874934+appu22@users.noreply.github.com>
2021-05-26 13:43:29 +0530

    prime and fact

--- PrimeFact.java
+++ PrimeFact.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 public class PrimeFact{
 	public static void main(String a[]){
 		
-		System.out.println("** Prime number ** ");
+		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Enter n number ");
 		int n =scanner.nextInt();
 		factorial(n);
+// 		 primeNumber(n)
 	}
-	public static void primeNumber(int n){		
+	public static void primeNumber(int n){	
+		System.out.println("** Prime number ** ");
 		boolean f=false;
 		for(int i = 2; i<=n/2; i++)
 		{
@@ -22,14 +24,15 @@ public class PrimeFact{
 			}
 		}
 		if(!f){
-			//true
+			 //false
 			System.out.println("is Prime number ");
 		}else{
-			//false
+			//true
 			System.out.println("not prime number ");
 		}
 	}//factorial == !5   5*4 5*3 5*2 5*1 = 120
 	public static void factorial(int n){
+		System.out.println("** Factorial  ** ");
 		int fact=1;
 		
 		for(int i=1; i<=n; i++){
